77fa221876f4c0803c3d14d18ea82ee01d8d4e48
Jackie <jackiemdillon@gmail.com>
2022-08-17 23:58:41 -0500

    modified:   package-lock.json 	modified:   package.json 	modified:   public/favicon.ico 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	modified:   public/style.css 	deleted:    src/App.css 	deleted:    src/App.js 	deleted:    src/App.test.js 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/setupTests.js 	new file:   src/style.css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,104 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+var express = require('express');
+var app     = express();
+var cors    = require('cors');
+var dal     = require('./dal.js');
+const e = require('express');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// used to serve static files from public directory
+app.use(express.static('public'));
+app.use(cors());
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// create user account
+app.get('/account/create/:name/:email/:password', function (req, res) {
+
+    // check if account exists
+    dal.find(req.params.email).
+        then((users) => {
+
+            // if user exists, return error message
+            if(users.length > 0){
+                console.log('User already in exists');
+                res.send('User already in exists');    
+            }
+            else{
+                // else create user
+                dal.create(req.params.name,req.params.email,req.params.password).
+                    then((user) => {
+                        console.log(user);
+                        res.send(user);            
+                    });            
+            }
+
+        });
+});
+
+
+// login user 
+app.get('/account/login/:email/:password', function (req, res) {
+
+    dal.find(req.params.email).
+        then((user) => {
+
+            // if user exists, check password
+            if(user.length > 0){
+                if (user[0].password === req.params.password){
+                    res.send(user[0]);
+                }
+                else{
+                    res.send('Login failed: wrong password');
+                }
+            }
+            else{
+                res.send('Login failed: user not found');
+            }
+    });
+    
+});
+
+// find user account
+app.get('/account/find/:email', function (req, res) {
+
+    dal.find(req.params.email).
+        then((user) => {
+            console.log(user);
+            res.send(user);
+    });
+});
+
+// find one user by email - alternative to find
+app.get('/account/findOne/:email', function (req, res) {
+
+    dal.findOne(req.params.email).
+        then((user) => {
+            console.log(user);
+            res.send(user);
+    });
+});
+
+
+// update - deposit/withdraw amount
+app.get('/account/update/:email/:amount', function (req, res) {
+
+    var amount = Number(req.params.amount);
+
+    dal.update(req.params.email, amount).
+        then((response) => {
+            console.log(response);
+            res.send(response);
+    });    
+});
+
+// all accounts
+app.get('/account/all', function (req, res) {
+
+    dal.all().
+        then((docs) => {
+            console.log(docs);
+            res.send(docs);
+    });
+});
+
+
+// var port = 3000;
+app.listen(process.env.PORT);
+// console.log('Running on port:' + port)
