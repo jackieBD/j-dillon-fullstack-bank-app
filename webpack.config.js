875437b3cbf77443f5ebbe38baf5418c904f6e79
Jackie <jackiemdillon@gmail.com>
2022-08-18 02:48:52 -0500

    ads

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,15 @@
+
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: '/src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'output.js',
+  },
+};
+
 module.exports = {
     resolve: {
         fallback: {
