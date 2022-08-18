3b8fca35a1d8a863523ebd55923b215280685430
Jackie <jackiemdillon@gmail.com>
2022-08-18 02:56:47 -0500

    m

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,7 @@
-
-const path = require('path');
-
-module.exports = {
-  mode: 'development',
-  entry: '/src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'output.js',
-  },
-};
-
 module.exports = {
     resolve: {
         fallback: {
-            url: require.resolve("url/"),
-            path: false,
-            zlib: require.resolve("browserify-zlib")
+            "path" : require.resolve("path-browserify"),
         }
     }
 }
